73329504e509aa64286d3880caf369e1ed504b12
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-28 20:27:56 +0530

    Create 02_Variables.go

---
+++ 01I–Go_Basics/02_Variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring a variable using var keyword
+	var age int = 17
+
+	// Declaring a variable using short-hand syntax
+	name := "Go Learner"
+
+	// Printing variables
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+}
